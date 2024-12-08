79233060210395c384793ebed7a2ab743d630640
Michelitossilva <michelssilva@gmail.com>
2024-12-08 00:09:54 -0300

    Cprrigindo e atualizando código do projeto Ada Tech Python

--- README.md
+++ README.md
@@ -6,3 +6,44 @@ O primeiro desafio é aplicação do **Método Twist** para codificação e deco
 
 `nb_challenge1_python.ipynb`: notebook com enunciado e solução passo a passo.<br>
 `script_solution1.py`: script com a solução do problema. 
+
+### Modificações Realizadas no Código
+1. Tratamento de Caracteres Especiais
+A função de conversão agora trata corretamente os caracteres especiais, como:
+
+Espaço : Mapeado para o valor 28 para garantir que o espaço seja corretamente codificado e decodificado.
+Underscore ('_') : Mapeado para o valor 0 para ser corretamente tratado.
+Ponto ('.') : Mapeado para o valor 27, mantendo a funcionalidade correta para mensagens com pontuação.
+2. Correção da Lógica de Conversão
+Funçãoconverte_char_int : Agora converta corretamente os caracteres para seus valores inteiros, incluindo os caracteres especiais e alfabéticos.
+Funçãoconverte_int_char : Realiza a conversão inversa, retornando os caracteres corretos com base nos inteiros processados.
+
+#### Como usar
+Executando o Código
+Codificação:
+
+Digite 0 para codificar a mensagem.
+Insira uma chave (número inteiro).
+Insira a mensagem a ser codificada.
+
+Exemplo codificar:
+
+python ./nome_do_arquivo.py
+Digite 0 para codificar e 1 para decodificar: 0
+Digite a chave (número inteiro): 5
+Digite a mensagem: esta funcionando
+Frase codificada: eemlpdzvwejcqfg.
+
+Decodificação:
+
+python ./nome_do_arquivo.py
+Digite 1 para decodificar a mensagem.
+Insira uma chave (número inteiro).
+Insira uma mensagem codificada.
+
+Exemplo decodificar:
+
+Digite 0 para codificar e 1 para decodificar: 1
+Digite a chave (número inteiro): 5
+Digite a mensagem: eemlpdzvwejcqfg.
+Frase decodificada: esta funcionando
--- python-challenge1/script_solution1.py
+++ python-challenge1/script_solution1.py
@@ -14,6 +14,9 @@ def converte_char_int(caractere):
     """
     if caractere == '.':
         return 27
+    
+    elif caractere == ' ':
+        return 28
 
     elif caractere == '_':
         return 0
@@ -22,7 +25,6 @@ def converte_char_int(caractere):
 
 
 def converte_int_char(inteiro):
-
     """Converte um inteiro para um caractere segundo a tabela ASCII
     com correção do valor do inteiro, segundo enunciado e uso da função chr(), para que:
 
@@ -36,6 +38,9 @@ def converte_int_char(inteiro):
     """
     if inteiro == 27:
         return '.'
+    
+    elif inteiro == 28:
+        return ' '
 
     elif inteiro == 0:
         return '_'
@@ -59,16 +64,17 @@ def codifica(lista_codigoplano, chave_k):
 
     for i in range(n_tam_msg):
         lista_cifradocodigo[i] = (lista_codigoplano[(chave_k * i) % n_tam_msg] - i) % 28
+    
     return lista_cifradocodigo
 
 
-def decodifica(lista_cifradocodigo,  chave_k):
+def decodifica(lista_cifradocodigo, chave_k):
     """A partir de lista_cifradocodigo aplica a função de decodificação segundo
     Método Twist
 
     Args:
         lista_cifradocodigo (list): lista com elementos de cifradocodigo
-        chave_k (int): chave para decoficação
+        chave_k (int): chave para decodificação
 
     Returns:
         [list]: lista com elementos que representam codigoplano
@@ -111,7 +117,6 @@ def to_string(lista_ref):
 
 
 def monta_lista_base(lista_ref):
-
     """A partir de uma lista_ref constrói uma nova lista
     de mesmo tamanho e elementos None
 
@@ -123,6 +128,7 @@ def monta_lista_base(lista_ref):
     """
     return [None]*len(lista_ref)
 
+
 ################ MAIN ################
 
 def main():
@@ -131,7 +137,7 @@ def main():
     """
 
     opcao = input('Digite 0 para codificar e 1 para decodificar: ')
-    chave_k = int(input('Digite a chave: '))
+    chave_k = int(input('Digite a chave (número inteiro): '))
     texto = input('Digite a mensagem: ')
 
     if opcao == '0':
@@ -140,7 +146,7 @@ def main():
         cifradocodigo = codifica(codigoplano, chave_k)
         lista_textocifrado = aplica_conversao(converte_int_char, cifradocodigo)
         textocifrado = to_string(lista_textocifrado)
-        print(f'Frase final: {textocifrado}')
+        print(f'Frase codificada: {textocifrado}')
 
     else:
         lista_textocifrado = list(texto)
@@ -148,7 +154,9 @@ def main():
         codigoplano = decodifica(cifradocodigo, chave_k)
         lista_textoplano = aplica_conversao(converte_int_char, codigoplano)
         textoplano = to_string(lista_textoplano)
-        print(f'Frase final: {textoplano}')
+        print(f'Frase decodificada: {textoplano}')
+
 
 if __name__ == '__main__':
     main()
+
